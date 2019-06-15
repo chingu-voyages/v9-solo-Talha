eb0e8a371851b1d33775784983a5cf72562d5bcf
Talha Mahmood <44188236+TalhaAWOL@users.noreply.github.com>
2019-06-15 13:28:43 -0400

    Added all images and main code for the app

---
+++ index.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
+		<link rel="icon" href="">
+
+		<title>
+      
+			Alix B. d'Anthenay - Marque de bijoux Made In France et digitale
+      
+		</title>
+		
+		<!-- jQuery CDN - Slim version (=with AJAX) -->
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+		<script src="http://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+		<!-- Popper.JS -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.4/js/tether.min.js"></script>
+		<!-- Bootstrap JS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+		<!-- Font Awesome Icons -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		
+		<style>
+		
+			body{
+				font-family: 'Roboto', sans-serif;
+				background-color:white;
+				font-weight: 300;
+				letter-spacing: -0.5px;
+			}
+
+			#navbar{
+				background-color:#292929;
+			}
+
+			.green{
+				color:#07B81F;
+			}
+
+			.greenanimate{
+				position:relative;
+				animation-name: greenanimate;
+				animation-duration: 3s;
+				animation-iteration-count: infinite;
+				color:#07B81F;
+			}
+			
+			@keyframes greenanimate {
+				0%   {
+					color:#07B81F;
+				}
+				50%  {
+					color:#5A5A5A;
+				}
+				100% {
+					color:#07B81F;
+				}
+			}
+
+			.gray{
+				color:#5A5A5A;
+			}
+
+			.grayanimate{
+				position:relative;
+				animation-name: grayanimate;
+				animation-duration: 3s;
+				animation-iteration-count: infinite;
+				color:#5A5A5A;
+			}
+			
+			@keyframes grayanimate {
+				0%   {
+					color:#5A5A5A;
+				}
+				50%  {
+					color:#07B81F;
+				}
+				100% {
+					color:#5A5A5A;
+				}
+			}
+
+			.bgBlack{
+				background-color:#202020;
+			}
+
+			.bgGray{
+				background-color:#7F7F7F;
+			}
+
+			.skinnyText{
+				font-weight:20;
+			}
+
+			.borderbotG{
+				border-bottom:3px solid #07B81F;
+			}
+
+			.mnegtop{
+				margin-top:-12px;
+			}
+
+			.offWhiteText{
+				color:#EEEAE0;
+			}
+
+			.center{
+				margin:auto;
+			}
+
+			.btn-circle {
+				width: 500px;
+				height: 50px;
+				text-align: center;
+				font-size: 12px;
+				line-height: 1.42857;
+			}
+
+		</style>
+
+	</head>
+
+	<body data-spy="scroll" data-target="#navbar">
+	
+		<nav id="navbar" class="navbar sticky-top navbar-expand-md navbar-dark">
+			<div class="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+				<ul class="navbar-nav mr-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#about">About</a>
+					</li>
+				</ul>
+			</div>
+			<div class="mx-auto order-0">
+				<a class="navbar-brand" href="">
+					<img src="pp.png" width="35px;" alt=""> <span class="greenanimate">Peer</span> <span class="grayanimate">Pals</span>
+				</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+			</div>
+			<div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link" href="#exe">Execution</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
+
+		<div id="about">
+			<div class="block block-inverse pb-5 block-secondary borderbotG bgBlack">
+				<div data-aos="fade-left" class="text-center">
+					<h1 class="block-title mb-0 app-myphone-brand text-white pt-5"><span class="green">Peer</span> <span class="gray">Pals</span> for IOS and Browsers</h1>
+					<p class="lead mt-5 mb-4 skinnyText text-white">Bringing you the simplest way to track progress and send out alerts to all your team members about slaicking! Offering a social media like platform to keep up with your group members</p>
+					<img class="img-fluid" width="800px" src="PortTech2.png" alt="">				
+				</div>
+			</div>
+			
+			<div class="block block-inverse pt-5 pb-5 block-secondary borderbotG bgGray">
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-12 col-sm-6 col-md-6">
+						
+							<p class="lead text-white">Schedule a group coding session</p>
+							<p class="offWhiteText mnegtop">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rhoncus ex ligula, eget finibus risus condimentum eget.</p>
+							<br>
+							<p class="lead text-white">One click to alert and email peers to</p>
+							<p class="offWhiteText mnegtop">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rhoncus ex ligula, eget finibus risus condimentum eget.</p>
+							<br>
+							<p class="lead text-white">Assign tasks and Track each members progress and total progress in one place</p>
+							<p class="offWhiteText mnegtop">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rhoncus ex ligula, eget finibus risus condimentum eget.</p>
+							<br>
+							<p class="lead text-white">Built in Calendar </p>
+							<p class="offWhiteText mnegtop">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi rhoncus ex ligula, eget finibus risus condimentum eget.</p>
+
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-6 text-center">
+						
+							<img src="progressapp.jpg" alt="" class="img-fluid mt-5">
+
+						</div>	
+					</div>
+				</div>
+			</div>
+		</div>
+
+		
+		<div class="block block-inverse pt-5 pb-5 block-secondary bgBlack">
+			
+			<div class="about textCenter center">
+				<div data-aos="fade-right" id="AWOLtext" class="h1 text-white text-center center"><span class="green">Exec</span><span class="gray">ution</span></div>
+			</div>
+			<div class="text-center center">
+				<p class="lead text-white mt-4">Using a hashtag enables you to interact with the main interface and execute commands provided by the application</p>
+				<p class="offWhiteText mnegtop mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+				<button id="reqbtn" name="reqbtn" type="submit" class="btn mt-3 btn-outline-success btn-circle btn-lg" style="width:130px;"><p style="font-size:20px">Visit App</p></button>
+			</div>
+
+		</div>
+		<div id="exe"></div>
+	
+		<script>
+
+			
+
+		</script>
+
+	</body>
+</html>
+
